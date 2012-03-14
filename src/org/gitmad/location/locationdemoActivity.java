86cfa9b91abbdf9a1f17eb18ac29bacdd35b4261
Bryan Sills <bryan.a.sills@gmail.com>
2012-03-14 01:05:04 -0400

    created most functionality. having issues with coarse accuracy

--- src/org/gitmad/location/locationdemoActivity.java
+++ src/org/gitmad/location/locationdemoActivity.java
@@ -1,7 +1,12 @@
 package org.gitmad.location;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class locationdemoActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +14,34 @@ public class locationdemoActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		final TextView tView = (TextView) findViewById(R.id.text);
+		tView.setText("Getting location...");
+        
+     // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+              // Called when a new location is found by the network location provider.
+              makeUseOfNewLocation(location);
+            }
+
+			public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+          };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
+    
+
+
+    private void makeUseOfNewLocation(Location location) {
+		TextView tView = (TextView) findViewById(R.id.text);
+		tView.setText("The location is: lat" + Double.toString(location.getLatitude()) + " long" + Double.toString(location.getLongitude()) + ".");
+	}
 }
